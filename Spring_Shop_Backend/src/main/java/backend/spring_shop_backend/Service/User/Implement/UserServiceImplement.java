7ea9118b9adb2d2b020a4a55d4f9747d9bbbacca
hanseongjun <gkstjdwns2@naver.com>
2025-07-27 13:58:33 +0900

    CREATE UserService.java, UserServiceImplement.java - User 서비스 생성

---
+++ Spring_Shop_Backend/src/main/java/backend/spring_shop_backend/Service/User/Implement/UserServiceImplement.java
@@ -0,0 +1,4 @@
+package backend.spring_shop_backend.Service.User.Implement;
+
+public class UserServiceImplement {
+}
---
+++ Spring_Shop_Backend/src/main/java/backend/spring_shop_backend/Service/User/UserService.java
@@ -0,0 +1,4 @@
+package backend.spring_shop_backend.Service.User;
+
+public interface UserService {
+}
