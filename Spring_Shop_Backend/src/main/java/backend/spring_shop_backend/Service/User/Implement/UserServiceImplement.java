14c8b32195e79c886532559cfc71cd2dbdb59e55
hanseongjun <gkstjdwns2@naver.com>
2025-07-27 14:26:01 +0900

    REFACT UserService.java, UserServiceImplement.java - 이메일 기반으로 사용자 찾기 기능 추가

--- Spring_Shop_Backend/src/main/java/backend/spring_shop_backend/Service/User/Implement/UserServiceImplement.java
+++ Spring_Shop_Backend/src/main/java/backend/spring_shop_backend/Service/User/Implement/UserServiceImplement.java
@@ -1,4 +1,27 @@
 package backend.spring_shop_backend.Service.User.Implement;
 
-public class UserServiceImplement {
+import backend.spring_shop_backend.Dto.UserDto;
+import backend.spring_shop_backend.Entity.User.User;
+import backend.spring_shop_backend.Repository.User.UserRepository;
+import backend.spring_shop_backend.Service.User.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserServiceImplement implements UserService, UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다. : " + email));
+    }
 }
--- Spring_Shop_Backend/src/main/java/backend/spring_shop_backend/Service/User/UserService.java
+++ Spring_Shop_Backend/src/main/java/backend/spring_shop_backend/Service/User/UserService.java
@@ -1,4 +1,11 @@
 package backend.spring_shop_backend.Service.User;
 
+import backend.spring_shop_backend.Dto.UserDto;
+import backend.spring_shop_backend.Entity.User.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface UserService {
+
+    public UserDetails loadUserByUsername(String email);
+
 }
