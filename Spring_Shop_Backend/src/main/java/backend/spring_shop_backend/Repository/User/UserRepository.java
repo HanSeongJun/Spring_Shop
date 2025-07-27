80d160bdd707b99bb91ff9a7e3f845873a925133
hanseongjun <gkstjdwns2@naver.com>
2025-07-27 13:58:11 +0900

    CREATE UserRepository.java - User 리포지토리 생성

---
+++ Spring_Shop_Backend/src/main/java/backend/spring_shop_backend/Repository/User/UserRepository.java
@@ -0,0 +1,14 @@
+package backend.spring_shop_backend.Repository.User;
+
+import backend.spring_shop_backend.Entity.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
